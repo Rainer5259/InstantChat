e61a05d73dae7dd4a5f1cf1401febc5020e6d543
Rainer <rainer_tribosk8@hotmail.com>
2023-08-19 00:37:17 -0300

    feat: add default colors

---
+++ src/components/theme/colors/index.ts
@@ -0,0 +1,8 @@
+const colors = {
+  background: {faintGray: '#f0f0f0', beige: '#ebe6da', beigeGray: '#a6a194'},
+  border: {paleBeigeGray: '#ccc5b4', black: '#000000'},
+  text: {skyBlue: '#8bb4f7', shadowColor: '#39414f', black: '#000000'},
+  shadowColor: {darkSlateGray: '#39414f'},
+  opacityRGBA: {beigeGray: 'rgba(166, 161, 148, 0.3)'},
+};
+export default colors;
