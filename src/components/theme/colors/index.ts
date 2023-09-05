2c81e9d70a80a45b7c16ebb5a33be0262477a16d
Rainer <rainer_tribosk8@hotmail.com>
2023-09-05 20:49:29 -0300

    feat: add icons & update file theme

--- src/components/theme/colors/index.ts
+++ src/components/theme/colors/index.ts
@@ -3,10 +3,15 @@ const colors = {
     defaultBeige: '#FFFAEC',
     mintGreen: '#DFECE4',
     ivory: '#F8F4E9',
+    periwinkle: '#8BB4F7',
   },
-  border: {lavender: '#AAA0D6', black: '#000000'},
+  border: {lavender: '#AAA0D6', black: '#000000', cornflowerBlue: '#70BAFF'},
   text: {indigo: '#3C178C', shadowColor: '#39414f', black: '#000000'},
   shadowColor: {darkSlateGray: '#39414f'},
-  opacityRGBA: {beigeGray: 'rgba(166, 161, 148, 0.3)'},
+  opacityRGBA: {
+    darkSlateGray: 'rgba(57,65,79,0.3)',
+    cottonCandyPink: 'rgba(252, 130, 255, 0.4)',
+    lavender: 'rgba(170, 160, 214, 0.6)',
+  },
 };
 export default colors;
--- src/components/theme/font/index.ts
+++ src/components/theme/font/index.ts
@@ -1,11 +1,16 @@
 const font = {
   weight: {
-    bold: 'bold',
+    regular: '600',
+    semiBold: '700',
+    bold: '800',
+    extraBold: '900',
   },
   family: {
     halvetica: 'halvetica',
+    sansation: 'sansation',
   },
   size: {
+    light: 12,
     small: 14,
     medium: 16,
     large: 18,
@@ -15,6 +20,10 @@ const font = {
     halveticaLight: {fontWeight: '200', fontFamily: 'Helvetica', fontSize: 16},
     halveticaBold: {fontWeight: '900', fontFamily: 'Helvetica', fontSize: 16},
     halveticaHeavy: {fontWeight: 'bold', fontFamily: 'Helvetica', fontSize: 18},
+    sansationSemiBold: {
+      fontFamily: 'sansation',
+      fontSize: 14,
+    },
   },
 };
 export default font;
