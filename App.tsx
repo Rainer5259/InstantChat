78a05304ce276dc7872e7695699c88afec45b8aa
Rainer <rainer_tribosk8@hotmail.com>
2023-09-15 19:46:59 -0300

    refactor: adjust safearea view on android

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Routes from './src/routes';
 import Toast from 'react-native-toast-message';
-import {SafeAreaView} from 'react-native';
+import {Platform, SafeAreaView} from 'react-native';
 import colors from './src/components/theme/colors';
 
 const App: JSX.ElementType = () => {
   return (
     <SafeAreaView
-      style={{
-        flex: 1,
-        backgroundColor: colors.background.defaultBeige,
-      }}>
+      style={[
+        {
+          flex: 1,
+          backgroundColor: colors.background.defaultBeige,
+        },
+        Platform.OS === 'android' && {paddingVertical: 20},
+      ]}>
       <Routes />
       <Toast visibilityTime={3000} autoHide topOffset={60} />
     </SafeAreaView>
