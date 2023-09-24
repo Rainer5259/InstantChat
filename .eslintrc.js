a5d2d50e3497a5f5f07922f0bc56b2b40dfb08df
Rainer <rainer_tribosk8@hotmail.com>
2023-09-24 17:31:44 -0300

    feat: add eslint style format and rules to project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,33 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
+    'no-use-before-define': ['error', { variables: false }],
+    'react/prop-types': ['error', { ignore: ['navigation', 'navigation.navigate'] }],
+  },
 };
